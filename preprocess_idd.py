76aa1bfa29e4bedca1818c9b1044bc2378d67f1c
Saijasi_sinha <saijasi.25bsa10021@vitbhopal.ac.in>
2025-12-01 13:55:10 +0530

    Add files via upload

---
+++ preprocess_idd.py
@@ -0,0 +1,6 @@
+
+def preprocess_idd():
+    print("\n---------------------------------------")
+    print(" Preprocessing skipped")
+    print(" Images already exist in dataset_traffic/processed/images")
+    print("---------------------------------------\n")
